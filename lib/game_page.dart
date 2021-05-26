2c5b715677112749c54af3affb0857024daec19e
0si43 <symn43@gmail.com>
2021-05-26 11:47:54 +0900

    要素を等間隔にスペーシングした

--- lib/game_page.dart
+++ lib/game_page.dart
@@ -11,12 +11,38 @@ class _GamePageState extends State<GamePage> {
   Widget build(BuildContext context) {
     var _numbers = List<int>.generate(74, (i) => i + 1);
     var _rand = new math.Random();
+    var _shape = const CircleBorder(
+      side: BorderSide(
+        color: Colors.black,
+        width: 1,
+        style: BorderStyle.solid,
+      ),
+    );
+
     return Scaffold(
       appBar: AppBar(
         title: Text("Game"),
       ),
-      body: Center(
-        child: Text(_numbers[_rand.nextInt(74)].toString()),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Center(
+            child: ElevatedButton(
+              child: Text(_numbers[_rand.nextInt(74)].toString()),
+              style: ElevatedButton.styleFrom(
+                fixedSize: Size(250.0, 250.0),
+                primary: Colors.white,
+                onPrimary: Colors.black,
+                shape: _shape,
+              ),
+              onPressed: () {
+                print("temp");
+              },
+            ),
+          ),
+          Divider(),
+          Text("temp"),
+        ],
       ),
     );
   }
