8d60cd3f11203e1483cadf565d467620863e4a12
0si43 <symn43@gmail.com>
2021-05-26 09:46:43 +0900

    dart:jsのimportが不要とワーニングが出ていたので削除した

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'dart:js';
-
 import 'package:bingo/first_page.dart';
 import 'package:flutter/material.dart';
 
